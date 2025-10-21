71a885f71c92b9599f13c20ecf96938b2518e1a4
Eben Edwyn <edwyn.eben@student.unamur.be>
2025-10-21 10:29:52 +0200

    Create PlayerCollision.java

---
+++ jpacman-framework/src/test/java/nl/tudelft/jpacman/level/PlayerCollision.java
@@ -0,0 +1,15 @@
+package nl.tudelft.jpacman.level;
+import nl.tudelft.jpacman.board.Direction;
+import nl.tudelft.jpacman.board.Square;
+import nl.tudelft.jpacman.npc.Ghost;
+import  nl.tudelft.jpacman.sprite.Sprite;
+public class PlayerCollision {
+
+
+    private PlayerCollision playerCollision;
+    private Player player;
+    private Ghost ghost;
+    private Pellet pellet;
+
+
+}
